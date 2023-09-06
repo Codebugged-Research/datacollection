12726e0a4bf9aede0414c125caae116175a71fda
kishor G <65946231+kishorsumathi@users.noreply.github.com>
2023-09-06 19:49:55 +0530

    Update mineral.py

--- src/margsoft/mineral.py
+++ src/margsoft/mineral.py
@@ -43,8 +43,9 @@ def collectdataset(rtsp,names_file,weight_file,cfg_file,path,dir_n,x_1,y_1,w_1,z
             print(f"CPU utilization: {cpu_percent}%")
             print(f"CPU utilization: {cpu_percent}%")
             ret, frame = cap.read()
+            frame=cv2.resize(frame, (900, 900))
             if poly:
-                frame_crop=frame
+                frame_crop=frame.copy()
                 cv2.fillPoly(frame_crop, [pts_left], 0)
                 cv2.fillPoly(frame_crop, [pts_right], 0)
             elif roi:
@@ -52,7 +53,7 @@ def collectdataset(rtsp,names_file,weight_file,cfg_file,path,dir_n,x_1,y_1,w_1,z
             else:
                 frame_crop=frame
             frame_crop= np.array(frame_crop)
-            frame=cv2.resize(frame, (900, 900))
+            # frame=cv2.resize(frame, (900, 900))
             frame = np.array(frame)
             classes, scores, boxes = model.detect(frame_crop, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
             for (classid, score, box) in zip(classes, scores, boxes):
