304af9669ca811f9f7d737ac9dc2257cf859b2ff
kishor G <65946231+kishorsumathi@users.noreply.github.com>
2023-12-06 17:35:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ SRC_REPO = "src"
 
 setuptools.setup(
     name="margsoft",
-    version="0.0.19",
+    version="0.0.20",
     author=AUTHOR_USER_NAME,
     description="data collection package",
     long_description=long_description,
